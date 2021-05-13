245ae9b88646582e82af62cbb0f4f41b2581e5dd
Parzival <parzivalnewton1@gmail.com>
2021-05-13 11:40:39 +0530

    Trees Problem 7

---
+++ Trees/leftClonedTree2.java
@@ -0,0 +1,11 @@
+class Solution{
+     
+       public static Node transBackFromLeftClonedTree(Node node){
+          if(node==null)return null;
+          node.left=transBackFromLeftClonedTree(node.left.left);
+          node.right=transBackFromLeftClonedTree(node.right);
+     
+          return node;
+        }
+
+}
