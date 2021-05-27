f95b47ac21a6ff8031e92798719a2431b2b7ee95
Parzival <parzivalnewton1@gmail.com>
2021-05-27 12:51:37 +0530

    DP 9

--- DynamicProgramming/coinChange.java
+++ DynamicProgramming/coinChange.java
@@ -1,10 +1,10 @@
 class Solution {
     public int change(int amount, int[] coins) {
         int[] dp=new int[amount+1];
-        for(int i=1;i<coins.length+1;i++){
-            for(int j=0;j<dp.length;j++){
-                if(j==0)dp[j]=1;
-                else if(coins[i-1]<=j)dp[j]=dp[j]+dp[j-coins[i-1]];
+        dp[0]=1;
+        for(int i=0;i<coins.length;i++){
+            for(int j=coins[i];j<dp.length;j++){
+                dp[j]=dp[j]+dp[j-coins[i]];
             }
         }
         return dp[amount];
