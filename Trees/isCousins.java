5dfa3ff05200ca0a4f0501db28946d7eecd00fa1
Parzival <parzivalnewton1@gmail.com>
2021-05-29 23:00:41 +0530

    Tree Problem 27

--- Trees/isCousins.java
+++ Trees/isCousins.java
@@ -5,27 +5,28 @@ class Solution {
         qt.add(root);
         while(qt.size()>0){
             int count=qt.size();
-            boolean fx=false,fy=false;
-            int px=0,py=0;
+            int px=-1,py=-1;
             for(int i=0;i<count;i++){
                 TreeNode temp=qt.remove();
                 if(temp.left!=null){
-                    if(temp.left.val==x){fx=true;px=temp.val;}
-                    else if(temp.left.val==y){fy=true;py=temp.val;}
+                    if(temp.left.val==x){px=temp.val;}
+                    else if(temp.left.val==y){py=temp.val;}
                     qt.add(temp.left);
                     
                 }
                 if(temp.right!=null){
-                    if(temp.right.val==x){fx=true;px=temp.val;}
-                    else if(temp.right.val==y){fy=true;py=temp.val;}
-                    qt.add(temp.right);
+                    if(temp.right.val==x){px=temp.val;}
+                    else if(temp.right.val==y){py=temp.val;}
+                     qt.add(temp.right);
                 }
                
             }
-            if(fx&&fy){
-                if(px==py)return false;
-                else return true;
-            }
+            
+            if(px>0&&py>0){
+              if(px!=py)return true;
+               else return false;
+            } 
+            if(px<0&&py>0||py<0&&px>0)return false;
             
             
         }
